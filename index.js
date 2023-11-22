6c2ae635bf85cdb89b2519d106fa8b4dd01c0b4c
Anny a mais linda <annylamberti2006@gmail.com>
2023-11-22 11:43:54 -0300

    Miau4 view proj

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require ("express")
-const exphbs = require ("express.handlebars")
+const exphbs = require ("express-handlebars")
 
 const app = express()
 
 app.engine('handlebars', exphbs.engine())
-app.set('view engine')
+app.set('view engine', 'handlebars')
 
 app.get('/', (requisicao, resposta) => {
-    resposta.send("No céu tem pão?")
+    resposta.render("home")
 })
 
 app.listen(3000, () => {
