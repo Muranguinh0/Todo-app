4320a53f0d935f191d3af02b5aadad928d1e9408
Anny a mais linda <annylamberti2006@gmail.com>
2023-11-22 10:34:52 -0300

    Miau2 exc server

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require ("express")
 
-const app = express(
-    
-)
+const app = express()
+
+app.get('/', (requisicao, resposta) => {
+    resposta.send("No céu tem pão?")
+})
+
+app.listen(3000, () => {
+    console.log("Miau rodando")
+})
